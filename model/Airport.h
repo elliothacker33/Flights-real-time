3255e5eadd55549af5a0c0938b75964c797cf2f3
Tomas Pereira <tomastorrespereira@gmail.com>
2023-07-01 14:35:25 +0100

    Working on Airports Models

---
+++ model/Airport.h
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Airport{
+public:
+	string getName() const;
+	string getIataCode() const;
+	string getIcaoCode() const;
+	Country* getCountry() const;
+	City* getCity() const;
+	double getLatitude() const;
+	double getLongitude() const;
+private:
+	string name;
+	string iata_code;
+	string icao_code;
+	Country* country;
+	City* city;
+	double lat;
+	double long;
+};
+
