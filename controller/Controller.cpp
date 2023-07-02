e3d123ec2ea480c89440a5d02531098950a26b51
Tomas Pereira <tomastorrespereira@gmail.com>
2023-07-02 16:12:42 +0100

    Added some logic to Controller

--- controller/Controller.cpp
+++ controller/Controller.cpp
@@ -1,48 +1,71 @@
 #include "Controller.h"
+#include <iostream>
+#include <string>
 #include <curl/curl.h>
-#include<fstream>
-#include<iostream>
 #include <nlohmann/json.hpp>
-using namespace std;
-using json = nlohmann::json;
 
-size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* response) {
+using json = nlohmann::json;
+Controller::Controller(){
+
+}
+// Callback Function for response data
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* response) const {
     size_t totalSize = size * nmemb;
     response->append(static_cast<char*>(contents), totalSize);
     return totalSize;
 }
-int main() {
-    CURL* curl;
-    CURLcode res;
-    std::string url = "https://airlabs.co/api/v9/countries?code=US&api_key=2446811c-3379-4690-a062-7a6302ad09bd";
-    std::string response;
 
-    curl_global_init(CURL_GLOBAL_DEFAULT);
-    curl = curl_easy_init();
-
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
-
-        res = curl_easy_perform(curl);
-
-        if (res != CURLE_OK) {
-            std::cerr << "Failed to perform request: " << curl_easy_strerror(res) << std::endl;
-        }
-
-        curl_easy_cleanup(curl);
-    }
-
-    curl_global_cleanup();
-
+// Function to process the JSON response
+void ProcessResponse(const std::string& response) {
     // Parse the response JSON
     json responseData = json::parse(response);
 
-    // Retrieve the county_code from the parsed JSON
-    std::string countyCode = responseData["county_code"];
+    // Retrieve the airlines array from the parsed JSON
+    json rows = responseData["rows"];
 
-    std::cout << "County Code: " << countyCode << std::endl;
+    // Iterate over the airlines and print the names
+    for (const auto& row : rows) {
+        std::cout << row["description"].get<std::string>() << std::endl;
+    }
+}
 
-    return 0;
+void Controller::callApi() const  {
+    CURL* curl = curl_easy_init();
+    if (curl) {
+        // Set the URL
+        curl_easy_setopt(curl, CURLOPT_URL, "https://flight-radar1.p.rapidapi.com/aircrafts/list");
+
+        // Set the custom request type to GET
+        curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "GET");
+
+        // Set the headers
+        struct curl_slist* headers = NULL;
+        headers = curl_slist_append(headers, "X-RapidAPI-Key: 78fbb0da29msh4839b0fc90f9f48p1b0d36jsn20d175b35c5d");
+        headers = curl_slist_append(headers, "X-RapidAPI-Host: flight-radar1.p.rapidapi.com");
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+        // Response data
+        std::string response;
+
+        // Set the write callback function to receive the response data
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION,WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+        // Perform the request
+        CURLcode res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK) {
+            std::cerr << "Failed to perform request: " << curl_easy_strerror(res) << std::endl;
+        } else {
+            // Process the response data
+            std::cout<<"hello";
+            ProcessResponse(response);
+        }
+
+        // Clean up
+        curl_easy_cleanup(curl);
+        curl_slist_free_all(headers);
+    } else {
+        std::cerr << "Failed to initialize cURL" << std::endl;
+    }
 }
--- controller/Controller.h
+++ controller/Controller.h
@@ -1,8 +1,25 @@
 #ifndef FLIGHTS_REAL_TIME_CONTROLLER_H
 #define FLIGHTS_REAL_TIME_CONTROLLER_H
 
+#include <cstddef>
+#include <string>
+#include<unordered_map>
+#include "Airline.h"
+#include "Airport.h"
+#include "Aircraft.h"
+
+using namespace std;
 
 class Controller {
+public:
+    Controller();
+    void callApi() const;
+
+private:
+    unordered_map<string,Airline*> airlines;
+    unordered_map<string,Airport*> airports;
+    unordered_map<string,Aircraft*> aircrafts;
+
 
 };
 
